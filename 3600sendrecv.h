b1bcd000cef13a4fb25e854585b12b46302991ec
Tony Huang <asdjkl@cs3600tcp.ccs.neu.edu>
2014-04-10 18:36:54 -0400

    Reset header definition temporarily for testing

--- 3600sendrecv.h
+++ 3600sendrecv.h
@@ -12,12 +12,11 @@
 #include <stdarg.h>
 
 typedef struct header_t {
-  unsigned int magic:15;
+  unsigned int magic:14;
+  unsigned int ack:1;
   unsigned int eof:1;
-  unsigned int ack:16;
-
-  unsigned int length:16;
-  unsigned int sequence:16;
+  unsigned short length;
+  unsigned int sequence;
 } header;
 
 unsigned int MAGIC;
